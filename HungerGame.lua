c6bc4c9f630336e117ba7b77ab50ab2fb691a59d
tp-sy <36451441+tp-sy@users.noreply.github.com>
2018-12-03 21:07:22 +0200

    Add files via upload

--- HungerGame.lua
+++ HungerGame.lua
@@ -5,7 +5,7 @@
 
 gMult= 8
 t = 1
-goldAmount = 1000
+goldAmount = 90
 cursor_x = 1
 cursor_y = 1
 
@@ -15,19 +15,24 @@ play_area_limit_y = 129
 flip = 0
 button_press = 0
 
+--Variables for Human menu --
+humanButton = 0
+sacId = 37
+
 -- Variables for building Menu --
 menu_screen = 0
 cursor_x_menu = 202
 cursor_y_menu = 10
 selectedId = 0
-
+selectedHuman = nil
 cursorScale = 1
 
 cost = {}
-cost[7] = 10
-cost[9] = 11
-cost[11] = 11
+cost[7] = 30
+cost[9] = 30
+cost[11] = 30
 
+tech_count = {0,0,0,0}
 --Variables for hunger and tech progress-
 hungerLevel = 10
 techLevel = 1
@@ -49,6 +54,7 @@ farmList = {}
 moneyh_count = 1
 school_count = 1
 farm_count = 1
+human_count = 0
 
 buildingObjects = {}
 bNumber = 1
@@ -119,7 +125,8 @@ function Human:new(age, sex, skills, posit)
                 size = 1;
                 direction = {0,0};
                 loc1 = fromPtoB(posit[1]) .. "," .. fromPtoB(posit[2]);
-                loc2 = fromPtoB(posit[1]) .. "," .. fromPtoB(posit[2])
+                loc2 = fromPtoB(posit[1]) .. "," .. fromPtoB(posit[2]);
+                state = 1
                 }
     self._index = self
     return setmetatable(infoTable, self)
@@ -171,7 +178,6 @@ function chooseDirection(obj)
       obj.direction = {0,0}
     else
       obj.direction = dir
-
       -- Point to correct sprites for directional movement --
       if obj.direction[2] == 1 and obj.sex == "female" then obj.spriteID = 35 end
       if obj.direction[2] == 1 and obj.sex == "male" then obj.spriteID = 3 end
@@ -182,7 +188,7 @@ function chooseDirection(obj)
       if obj.direction[1] == -1 and obj.sex == "female" then obj.spriteID = 52 end
       if obj.direction[1] == -1 and obj.sex == "male" then obj.spriteID = 51 end
       obj.loc2 = fromPtoB(obj.pos[1]) + obj.direction[1] .. "," .. fromPtoB(obj.pos[2]) + obj.direction[2]
-      mapList[obj.loc2] = nil
+      mapList[obj.loc2] = 0
     end
 end
 
@@ -208,17 +214,47 @@ function drawMap()
     spr(v.spriteID, v.pos[1], v.pos[2], 0, 1, 0, 0, 2, 2)
   end
   for i, v in pairs(humanList) do
-    spr(v.spriteID, v.pos[1], v.pos[2],0)
+    if v.state == 1 then spr(v.spriteID, v.pos[1], v.pos[2],0) end
   end
 end
 
 -- Updates the maplist to block moving on buildings --
 
 function updateMapList(obj)
-    mapList[fromPtoB(obj.pos[1])+1 .. "," .. fromPtoB(obj.pos[2])] = 2
+    for i, v in pairs(humanList) do
+      if v.loc1 == fromPtoB(obj.pos[1])+1 .. "," .. fromPtoB(obj.pos[2]) or v.loc2 == fromPtoB(obj.pos[1])+1 .. "," .. fromPtoB(obj.pos[2]) then
+        newPos = randomPosition()
+        v.pos = {fromBtoP(newPos[1]), fromBtoP(newPos[2])}
+        v.loc1 = newPos[1] .. "," .. newPos[2]
+        v.loc2 = newPos[1] .. "," .. newPos[2]
+        v.direction = {0,0}
+      end
+      if v.loc1 == fromPtoB(obj.pos[1])+1 .. "," .. fromPtoB(obj.pos[2])+1 or v.loc2 == fromPtoB(obj.pos[1])+1 .. "," .. fromPtoB(obj.pos[2])+1 then
+        newPos = randomPosition()
+        v.pos = {fromBtoP(newPos[1]), fromBtoP(newPos[2])}
+        v.loc1 = newPos[1] .. "," .. newPos[2]
+        v.loc2 = newPos[1] .. "," .. newPos[2]
+        v.direction = {0,0}
+      end
+      if v.loc1 == fromPtoB(obj.pos[1]) .. "," .. fromPtoB(obj.pos[2])+1 or v.loc2 == fromPtoB(obj.pos[1]) .. "," .. fromPtoB(obj.pos[2])+1 then
+        newPos = randomPosition()
+        v.pos = {fromBtoP(newPos[1]), fromBtoP(newPos[2])}
+        v.loc1 = newPos[1] .. "," .. newPos[2]
+        v.loc2 = newPos[1] .. "," .. newPos[2]
+        v.direction = {0,0}
+      end
+      if v.loc2 == fromPtoB(obj.pos[1]) .. "," .. fromPtoB(obj.pos[2]) or v.loc2 == fromPtoB(obj.pos[1]) .. "," .. fromPtoB(obj.pos[2]) then
+        newPos = randomPosition()
+        v.pos = {fromBtoP(newPos[1]), fromBtoP(newPos[2])}
+        v.loc1 = newPos[1] .. "," .. newPos[2]
+        v.loc2 = newPos[1] .. "," .. newPos[2]
+        v.direction = {0,0}
+      end
+    end
     mapList[fromPtoB(obj.pos[1])+1 .. "," .. fromPtoB(obj.pos[2])+1] = 2
     mapList[fromPtoB(obj.pos[1]) .. "," .. fromPtoB(obj.pos[2])+1] = 2
     mapList[fromPtoB(obj.pos[1]) .. "," .. fromPtoB(obj.pos[2])] = 2
+    mapList[fromPtoB(obj.pos[1])+1 .. "," .. fromPtoB(obj.pos[2])] = 2
 end
 
 
@@ -250,7 +286,7 @@ function cursor_buttons()
   end
   if btn(a) then selectedId = 0 end
   if selectedId == 1 and btn(z) then
-    if goldAmount - cost[buildId] > 0 then
+    if goldAmount - cost[buildId] > 0 and mapList[fromPtoB(cursor_x) .. "," .. fromPtoB(cursor_y)] < 2 and mapList[fromPtoB(cursor_x) + 1 .. "," .. fromPtoB(cursor_y)] < 2 and mapList[fromPtoB(cursor_x) .. "," .. fromPtoB(cursor_y) + 1] < 2 then
       buildingList["B" .. bNumber] = Building:new(buildId, {cursor_x, cursor_y})
       updateMapList(buildingList["B" .. bNumber])
       bNumber = bNumber + 1
@@ -271,8 +307,8 @@ function cursor_menu_buttons()
   end
   if btn(a) then menu_screen = 0 end
   if btn(z) then
-    selectedId = 1
     menu_screen = 0
+    selectedId = 1
     if cursor_y_menu == 10 then
         buildId = 7
     end
@@ -284,6 +320,20 @@ function cursor_menu_buttons()
     end
     button_press = 1
   end
+end
+
+function human_cursor_buttons()
+    if btn(a) and button_press == 0 then
+        humanButton = 0
+        selectedHuman = nil
+        button_press = 1
+    end
+    if btn(x) and button_press == 0 then
+        killHuman(selectedHuman)
+        selectedHuman = nil
+        humanButton = 0
+        button_press = 1
+    end
 end
 
 sizeItem = 4
@@ -307,11 +357,11 @@ function drawMenuCursor()
 end
 
 function gold()
-		goldAmount = goldAmount + 5
+		goldAmount = goldAmount + 1 + (moneyh_count)/5
 end
 
 function hungerLevelManager(hungerCount)
-    hungerLevel = hungerLevel + (1/farm_count)*hungerCount
+    hungerLevel = hungerLevel + (1*hungerCount - 0.25*human_count + 0.25*farm_count)/5
     if hungerLevel < 5 then
         hungerLevel_color = 6
     end
@@ -323,15 +373,38 @@ function hungerLevelManager(hungerCount)
 end
 
 function techLevelManager(techCount)
-    techProgress = techProgress + techCount
-    if techProgress >= 30 then
+    techProgress = techProgress + techCount + 0.25*school_count
+    if techProgress >= 30*techLevel then
         techProgress = 0
         techLevel = techLevel + 1
     end
 end
 
-function addHuman()
+function randomPosition()
+    repeat
+      rand_x = math.random(0,23)
+      rand_y = math.random(0,15)
+    until mapList[rand_x .. "," .. rand_y] == 1
+    return {rand_x, rand_y}
+end
 
+function killHuman(target)
+  mapList[target.loc1] = 1
+  mapList[target.loc2] = 1
+  target.state = 0
+  human_count = human_count - 1
+end
+
+function techLevels()
+  for i, v in pairs(humanList) do
+    if v.skill == "Red" then tech_count[1] = tech_count[1]+1 end
+    if v.skill == "Blue" then tech_count[2] = tech_count[2]+1 end
+    if v.skill == "Green" then tech_count[3] = tech_count[3]+1 end
+    if v.skill == "Gold" then tech_count[4] = tech_count[4]+1 end
+  end
+end
+
+function addHuman()
     genderList = {"male", "female"}
     colors = {"Red", "Blue", "Green", "Gold"}
     choiceG = math.random(1, 2)
@@ -341,9 +414,8 @@ function addHuman()
       random_y_Pos = math.random(0,15)
     until mapList[random_x_Pos .. "," .. random_y_Pos] == 1
     humanList["H" .. hNumber] = Human:new(1, genderList[choiceG], colors[choiceC], {fromBtoP(random_x_Pos), fromBtoP(random_y_Pos)})
-
     hNumber = hNumber + 1
-
+    human_count = human_count + 1
 end
 
 -- Test Objects --
@@ -354,22 +426,35 @@ function TIC()
       if not btn(0) and not btn(1) and not btn(2) and not btn(3) and not btn(z) and not btn(x) and not btn(a) then
         button_press = 0
       end
-      if btn(x) and button_press == 0 then menu_screen = 1 end
+      if btn(z) and button_press == 0 and menu_screen == 0 and selectedId == 0 then
+            if mapList[fromPtoB(cursor_x) .."," .. fromPtoB(cursor_y)] == 0 then
+                for i, v in pairs(humanList) do
+                  if v.loc1 == fromPtoB(cursor_x) .."," .. fromPtoB(cursor_y) or v.loc2 == fromPtoB(cursor_x) .."," .. fromPtoB(cursor_y) then
+                    selectedHuman = v
+                  end
+                end
+                humanButton = 1
+            end
+      end
+      techLevels()
       if button_press == 0 and menu_screen == 0 then cursor_buttons() end
+      if btn(x) and button_press == 0 and humanButton == 0 then menu_screen = 1 end
       cls(5)
        map()
        rect(0, play_area_limit_y, 240, 8, 2)
        rect(play_area_limit_x, 0, 80, 136, 2)
        -- Prints screen for Building interface panel
-       if menu_screen == 0 then
+       if menu_screen == 0 and humanButton == 0 then
 
-           spr(32, 204, 4, 0, 3, 0, 0, 1, 1)
-           spr(48, 204, 28, 0, 3, 0, 0, 1, 1)
-           spr(64, 204, 52, 0, 3, 0, 0, 1, 1)
-           spr(80, 204, 76, 0, 3, 0, 0, 1, 1)
+           spr(32, 208, 4, 0, 3, 0, 0, 1, 1)
+           spr(48, 208, 28, 0, 3, 0, 0, 1, 1)
+           spr(64, 208, 52, 0, 3, 0, 0, 1, 1)
+           spr(80, 208, 76, 0, 3, 0, 0, 1, 1)
            -- Prints numbers alive, of each ability
-           print(4 .."x", 193, 13)
-
+           print(tech_count[1] .."x", 194, 13)
+           print(tech_count[2] .."x", 194, 13+3*8)
+           print(tech_count[3] .."x", 194, 13+6*8)
+           print(tech_count[4] .."x", 194, 13+9*8)
        end
        if menu_screen == 1 then
 
@@ -387,18 +472,38 @@ function TIC()
            rect(220, 28, 10, 10, 1)
            rect(220, 63, 10, 10, 1)
            rect(220, 98, 10, 10, 1)
-           print(10, 220, 31, 14)
-           print(10, 220, 66, 14)
-           print(10, 220, 101, 14)
+           print(30, 220, 31, 14)
+           print(30, 220, 66, 14)
+           print(30, 220, 101, 14)
 
 
            if button_press == 0 then
                cursor_menu_buttons()
            end
        end
+       if humanButton == 1 then
+            skillS = selectedHuman.skills
+            ageS = selectedHuman.age
+            print("Age: " .. ageS, 200, 8+35)
+            print("Skill: ", 200, 16+35)
+            print(skillS, 200, 24+35)
+            -- Draws skills in
+            --[[for h=1, skillOwned, 1 do
+                spr(skillSpritePos, 200, 8+(h*35), 0, 2, 0, 0, 2, 2)
+            end --]]
+
+            --[[if t%90 == 0 then
+                sacId = 37
+            elseif t%45 == 0 then
+                sacId = 39 --]]
+            --spr(sacId, 80, 50, 0, 2, 0, 0, 2, 2)
+            --end
+            --print("PRESS X TO SACRIFICE", 10, 22)
+            human_cursor_buttons()
+       end
+
        print("Score:", 0, 130)
-       print("Tech:", 50, 130)
-       print("Gold: " .. goldAmount, 90, 130, 9)
+       print("Gold: " .. math.floor(goldAmount), 90, 130, 9)
 
        --Prints Hunger Level
        rect(200, 112, 32, 7, 1)
@@ -406,11 +511,8 @@ function TIC()
 
        --Prints Tech level
        rect(200, 120, 32, 7, 1)
-       rect(201, 121, techProgress, 5, 8)
+       rect(201, 121, techProgress/techLevel, 5, 8)
        print(techLevel, 194, 121)
-
-
-
        print(cursor_y_menu, 0,0, 1)
        --Draws Different Cursors
        drawMap()
@@ -420,23 +522,18 @@ function TIC()
        drawCursor()
        if menu_screen == 1 then drawMenuCursor() end
 
-       techLevelManager(techSpeed *4  / 60)
-       if techLevel >= 3 then
+       if t%600 == 0 then techLevelManager(2) end
+       if techLevel > 3 then
            techSpeed = 0
-
-           --print("GAME IS OVER HIT THE SHOWER", 40, 40)
+           print("GAME IS OVER HIT THE SHOWER", 40, 40)
        end
 
-       if t%60 == 0 then
-         hungerLevelManager(-(1/2))
-       end
+       if t%60 == 0 then hungerLevelManager(-1) end
 
-       if t%180 == 0 then
-         gold()
-       end
+       if t%60 == 0 then gold() end
        updateAge()
-       if t%30 == 0 then addHuman() end
-       if t%2 == 0 then moveHuman() end
+       if t%300 == 0 then addHuman() end
+       if t%12 == 0 then moveHuman() end
        t = t+1
        if t%60 == 0 then
            timeCounter = timeCounter + 1
